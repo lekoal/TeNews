e277939cccb3108deabb6ba8133f39675e76d08d
lekoal <coreglus@gmail.com>
2023-04-05 09:20:18 +0300

    Rss date formatter is added

---
+++ app/src/main/java/com/private_projects/tenews/utils/RssDateFormatter.kt
@@ -0,0 +1,21 @@
+package com.private_projects.tenews.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class RssDateFormatter {
+    private val inputFormatA = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH)
+    private val inputFormatB = SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH)
+    private val outputFormat = SimpleDateFormat("HH:mm dd.MM.yyyy", Locale.getDefault())
+    fun format(date: Date): String {
+        return try {
+            val inputDate = inputFormatA.parse(date.toString())
+            val outputDate = inputDate?.let { outputFormat.format(it) }
+            outputDate.toString()
+        } catch (e: Exception) {
+            val inputDate = inputFormatB.parse(date.toString())
+            val outputDate = inputDate?.let { outputFormat.format(it) }
+            outputDate.toString()
+        }
+    }
+}
